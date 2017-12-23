bd9b9f48111971f1f725a915c2e852f53ffb0ce9
Nehal Vaidya <nv14@ilch-ml0190.lan>
2017-12-23 14:22:22 -0600

    some text changes

--- src/index.test.js
+++ src/index.test.js
@@ -5,3 +5,10 @@ describe('Our first test', () => {
     expect(true).toEqual(true);
   });
 });
+
+
+describe('Our second test', () => {
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
